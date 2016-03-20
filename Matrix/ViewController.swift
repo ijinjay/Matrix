a83d13620cfd190382038359d9a537936c7342a2
JinJay <jinjaysnow@gmail.com>
2016-03-20 19:42:40 +0800

    Initial Commit

---
+++ Matrix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Matrix
+//
+//  Created by Jay on 16/3/20.
+//  Copyright © 2016年 Jay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
